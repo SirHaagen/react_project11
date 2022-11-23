53dc65d2e739002dc0506c5de78180094565ffff
Fabian Leon <96808937+SirHaagen@users.noreply.github.com>
2022-11-23 18:11:42 -0500

    Add files via upload

---
+++ 011_App.jsx
@@ -0,0 +1,13 @@
+
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+
+const App = () => {
+
+  return <>
+    <TaskForm />
+    <TaskList />
+  </>
+}
+
+export default App
